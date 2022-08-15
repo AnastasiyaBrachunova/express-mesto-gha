354d69d4dcfd6c971775fe49bad4a0d8bef0dec2
Anastasiya Brachunova <abrachunova@yandex.ru>
2022-08-15 23:19:29 +0300

    666

---
+++ controllers/cards.js
@@ -0,0 +1,12 @@
+const card = require("../models/card") // экспортироали модель карточки
+const getCards =(req, res) =>{}
+const createCards =(req, res) =>{}
+const deleteCard =(req, res) =>{}
+
+
+const changeUserInfo =(req, res) =>{}
+const changeAvatar =(req, res) =>{}
+const likeCard =(req, res) =>{}
+const dislikeCard =(req, res) =>{}
+
+module.exports = { changeUserInfo, changeAvatar, likeCard, dislikeCard, getCards, createCards, deleteCard };
---
+++ routes/cards.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+const { changeUserInfo, changeAvatar, likeCard, dislikeCard, getCards, createCards, deleteCard } = require('../controllers/cards')
+
+router.get('/cards', getCards)  // возвращает все карточки
+router.post('/cards', createCards)  // создает карточку
+router.delete('/cards/:id', deleteCard)  // удаляеn карточку по айди
+
+
+
+router.patch('/users/me', changeUserInfo)  // обновление профиля
+router.patch('/users/me/avatar', changeAvatar)  // бновление аватара
+
+router.put('/cards/:id/likes', likeCard) // лайк карточки
+router.delete('/cards/:id/likes', dislikeCard) // дизлайк карточки
+
+module.exports = router;
