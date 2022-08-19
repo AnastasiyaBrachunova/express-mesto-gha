f55d0c68ecb171efd3c4f5ab8df5dd49e00303ba
Anastasiya Brachunova <abrachunova@yandex.ru>
2022-08-20 02:52:43 +0300

    2 попытка автотетст

--- controllers/users.js
+++ controllers/users.js
@@ -23,16 +23,23 @@ const createUser = (req, res) => User.create(req.body)
 //       res.status(500).send({ message: `Internal server error ${error}` });
 //     }
 //   });
-const getUser = (req, res, next) => User.findById(req.params._id)
+const getUser = (req, res) => User.findById(req.params._id)
+  .orFail(() => {
+    const error = new Error('Пользователь с указанным _id не найден');
+    error.statusCode = 404;
+    throw error;
+  })
   .then((user) => res.status(200).res.send(user))
   .catch((err) => {
-    if (err === 'ValidatorError') {
-      return next(err);
+    if (err.name === 'CastError') {
+      res.status(400).send({ message: `Переданы некорректные данные при создании пользователя ${err}` });
+    } else {
+      res.status(500).send({ message: `Internal server error ${err}` });
     }
-    res.status(500).send({ message: `Internal server error ${err}` });
   });
 
 const getUsers = (req, res) => User.find({})
+
   .then((users) => res.status(200).send(users))
   .catch((error) => {
     res.status(500).send({ message: `Internal server error ${error}` });
