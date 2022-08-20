8ddfd6bfb6937332fe84208c7c8d52c4e15b4add
Anastasiya Brachunova <abrachunova@yandex.ru>
2022-08-20 03:13:41 +0300

    3 попытка автотетст

--- models/card.js
+++ models/card.js
@@ -1,34 +1,39 @@
 const mongoose = require('mongoose');
 
-const cardSchema = new mongoose.Schema({
-  name: {
-    type: String, // имя — это строка
-    required: true, // оно должно быть у каждого пользователя, так что имя — обязательное поле
-    minlength: 2, // минимальная длина имени — 2 символа
-    maxlength: 30, // а максимальная — 30 символов
-  },
-  link: {
-    type: String,
-    required: true,
-    validate: {
-      validator(url) {
-        const urlRegex = /https?:\/\/\S+\.\S+/gm;
-        return urlRegex.test(url);
+const cardSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String, // имя — это строка
+      required: [true, 'Поле "name" должно быть заполнено'], // оно должно быть у каждого пользователя, так что имя — обязательное поле
+      minlength: 2, // минимальная длина имени — 2 символа
+      maxlength: 30, // а максимальная — 30 символов
+    },
+    link: {
+      type: String,
+      required: true,
+      validate: {
+        validator(url) {
+          const urlRegex = /https?:\/\/\S+\.\S+/gm;
+          return urlRegex.test(url);
+        },
       },
     },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    likes: [{
+      type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
+      default: [],
+    }],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
+  {
+    versionKey: false,
   },
-  likes: [{
-    type: mongoose.Schema.Types.ObjectId,
-    default: [],
-  }],
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+);
 
 module.exports = mongoose.model('card', cardSchema);
