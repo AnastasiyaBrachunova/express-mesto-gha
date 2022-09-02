73e2d2d7c20f5092d18843e95e419f5d05fa3166
Anastasiya Brachunova <abrachunova@yandex.ru>
2022-09-02 23:11:10 +0300

    ревью 1 попыткfa 5

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,20 +1,20 @@
 const jwt = require('jsonwebtoken');
 const AuthorizationError = require('../errors/AuthorizationError');
 
-
 // eslint-disable-next-line consistent-return
 const auth = (req, res, next) => {
   if (!req.cookies.jwt) {
-    next(new AuthorizationError('Ошибка авторизации'))
+    next(new AuthorizationError('Ошибка авторизации'));
   }
 
   const token = req.cookies.jwt;
-
   let payload;
+
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    next(new AuthorizationError('Ошибка авторизации'))
+    next(new AuthorizationError('Ошибка авторизации'));
+  }
 
   req.user = payload; // записываем пейлоуд в объект запроса
 
