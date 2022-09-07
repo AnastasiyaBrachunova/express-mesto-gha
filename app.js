bd26ae6ac933f8f416a528472b237e8a993e18dd
Anastasiya Brachunova <abrachunova@yandex.ru>
2022-09-08 01:08:08 +0300

    мидлвара корс

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require('express'); // импортировали экспрес
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger'); // ИМПОРТ ЛОГОВ
+
 const authRouter = require('./routes/auth');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
@@ -18,14 +21,33 @@ const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
+const options = {
+  origin: [
+    'http://localhost:порт',
+    'https://ВАШ ДОМЕЙН С ДОКУМЕНТА',
+    'https://YOUR.github.io',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
+
+app.use('*', cors(options));
+
 app.use(express.json({ type: '*/*' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use(requestLogger); // ЛОГГЕР ЗАПРОСОВ
 
 app.use('/', authRouter); // здесь роуты signup/signin
 app.use(auth); // защита роутов авторизацией
 app.use('/', usersRouter);
 app.use('/', cardsRouter);
+
+app.use(errorLogger); // ЛОГГЕР ОШИБОК
 
 app.use((req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
