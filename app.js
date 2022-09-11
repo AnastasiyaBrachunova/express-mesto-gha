6d53ad3db3ed96a8bfa8f1f9012892d17f8b4727
Anastasiya Brachunova <abrachunova@yandex.ru>
2022-09-11 19:34:02 +0300

    add options

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const options = {
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
   preflightContinue: false,
   optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  allowedHeaders: ['Accept', 'X-Requested-With', 'Content-Type', 'Origin', 'Authorization'],
   credentials: true,
 };
 
