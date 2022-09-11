09a76a57fecdd52d081d092e0609887e326f4ba3
Anastasiya Brachunova <abrachunova@yandex.ru>
2022-09-11 19:48:47 +0300

    add options

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express'); // импортировали экспрес
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
-const cors = require('cors');
+// const cors = require('cors');
 
 const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger'); // ИМПОРТ ЛОГОВ
@@ -23,22 +23,46 @@ const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-const options = {
-  origin: [
-    'localhost:3010', // порт где крутится фронт
-    'https://abrachunova.front.nomoredomains.sbs', // созданный домен для фронта
-    'https://AnastasiyaBrachunova.github.io', // мой личный акк гита?
-  ],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: ['Accept', 'X-Requested-With', 'Content-Type', 'Origin', 'Authorization'],
-  credentials: true,
-};
+// const options = {
+//   origin: [
+//     'localhost:3010', // порт где крутится фронт
+//     'https://abrachunova.front.nomoredomains.sbs', // созданный домен для фронта
+//     'https://AnastasiyaBrachunova.github.io', // мой личный акк гита?
+//   ],
+//   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204,
+//   allowedHeaders: ['Accept', 'X-Requested-With', 'Content-Type', 'Origin', 'Authorization'],
+//   credentials: true,
+// };
 
-app.use('*', cors(options));
+// app.use('*', cors(options));
+
+// app.use(express.json({ type: '*/*' }));
+
+const allowedCors = [
+  'https://abrachunova.front.nomoredomains.sbs',
+  'https://AnastasiyaBrachunova.github.io',
+  'localhost:3010'];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS');
+  }
+
+  if (req.method === 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
+});
 
 app.use(express.json({ type: '*/*' }));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
